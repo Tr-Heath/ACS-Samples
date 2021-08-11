f930a22571a5a9e4a1b9ac4f43188e8f29ffd4c9
Travis Heath <travis.heath@b3groupinc.com>
2021-08-10 20:57:07 -0700

    Fix bug with key retrieval and clean up testing console output

--- LanguageDetection.py
+++ LanguageDetection.py
@@ -5,27 +5,26 @@ from urllib import request, parse, error
 from azure.keyvault.secrets import SecretClient
 from azure.identity import AzureCliCredential
 
+
 def main():
     global cog_endpoint
     global cog_key
 
     try:
-        #Get configuration settings
+        load_dotenv()
         cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
+        #Get configuration settings from KeyVault
+        #Another option would be to include the API key in the .env file with the other names
+    
         cog_keyname = os.getenv('COG_SERVICE_KEYNAME')
-
-        print(cog_endpoint)
 
         #get actual key value out of Azure Key Vault
         keyVaultName = os.environ["KEY_VAULT_NAME"]
+
         keyVaultURI = f"https://{keyVaultName}.vault.azure.net"
         credential = AzureCliCredential()
         client = SecretClient(vault_url=keyVaultURI, credential=credential)
         cog_key = client.get_secret(cog_keyname)
-
-        print(f"KeyVaultName: {keyVaultName}")
-        print(f"KeyName: {cog_keyname}")
-        print(f"KeyValue: {cog_key}")
 
         userText = ""
         #Get repeated user input until user exit flag
@@ -37,6 +36,8 @@ def main():
         print(ex)
 
 def GetLanguage(userText):
+    """Build a request json from the user input and open a connection to the API endpoint
+    configured in this module."""
     try:
         #collection of documents with an id and text value
         requestBody = {
@@ -46,30 +47,21 @@ def GetLanguage(userText):
             ]
         }
 
-        print(json.dumps(requestBody, indent=2))
-
         uri = cog_endpoint.rstrip('/').replace('https://', '')
         conn = http.client.HTTPSConnection(uri)
-        print("connection made")
 
         headers = {
             'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': cog_key
-        }
+            'Ocp-Apim-Subscription-Key': cog_key.value
+        }      
 
-        print(str(requestBody).encode("utf-8"))
-        
-
-        conn.request("POST", "/text/analytics/v3.0/languages?", str(requestBody).encode("utf-8"), headers)
+        conn.request("POST", "/text/analytics/v3.0/languages?", str(requestBody).encode('utf-8'), headers)
         response = conn.getresponse()
-        print("got response")
         data = response.read().decode("utf-8")
-        print("got data")
 
         if response.status == 200:
             results = json.loads(data)
 
-            print(json.dumps(requestBody, indent=2))
             for document in results["documents"]:
                 print("\nLanguage: ", document["detectedLanguage"]["name"])
         #else we did not get 200 and have an error, print the data we did get if anything
